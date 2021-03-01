ab4eaa909505a053ac7bbfd31aad4328b5b2025c
nhu-pham352 <nhu.pham352@g.austincc.edu>
2021-02-28 23:05:23 -0600

    HW3

--- src/classwork/03_assign/decision.cpp
+++ src/classwork/03_assign/decision.cpp
@@ -1,1 +1,71 @@
 //cpp
+#include"decision.h"
+#include<iostream>
+
+
+std::string get_letter_grade_using_if(int grade)
+{
+    std::string letter_grade;
+
+    if (grade>89 && grade<101)
+    {
+        letter_grade = "A";
+    }
+    else if (grade>79 && grade<90)
+    {
+        letter_grade = "B";
+    }
+    else if (grade>69 && grade<80)
+    {
+        letter_grade = "C";
+    }
+    else if (grade>59 && grade<70)
+    {
+        letter_grade = "D";
+    }
+    else
+    {
+        letter_grade = "F";
+    }
+    
+    return letter_grade;
+}
+
+std::string get_letter_grade_using_switch(int grade)
+{
+    std::string return_grade;
+    grade = grade / 10;
+
+    switch (grade)
+    {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        return_grade = "F";
+        break;
+    case 6:
+        return_grade = "D";
+        break;
+    case 7:
+        return_grade = "C";
+        break;
+    case 8:
+        return_grade = "B";
+        break;
+    case 9:
+        return_grade = "A";
+        break;
+    case 10:
+        return_grade = "A";
+        break;
+    default:
+        return_grade = "Invalid grade";
+        break;
+    
+    return return_grade;
+    }
+}
+
--- test/classwork_test/03_assign_test/decisions_tests.cpp
+++ test/classwork_test/03_assign_test/decisions_tests.cpp
@@ -1,8 +1,27 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "loops.h"
+#include"decision.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
+TEST_CASE("Test convert numerial grade to a letter grade using if function")
+{
+	REQUIRE(get_letter_grade_using_if(98) == "A");
+	REQUIRE(get_letter_grade_using_if(85) == "B");
+	REQUIRE(get_letter_grade_using_if(75) == "C");
+	REQUIRE(get_letter_grade_using_if(65) == "D");
+	REQUIRE(get_letter_grade_using_if(50) == "F");
 
+}
+
+TEST_CASE("Test convert numerial grade to a letter grade using switch function")
+{
+	REQUIRE(get_letter_grade_using_switch(98) == "A");
+	REQUIRE(get_letter_grade_using_switch(85) == "B");
+	REQUIRE(get_letter_grade_using_switch(75) == "C");
+	REQUIRE(get_letter_grade_using_switch(65) == "D");
+	REQUIRE(get_letter_grade_using_switch(50) == "F");
+
+}
