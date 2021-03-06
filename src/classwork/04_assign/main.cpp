d764dabe5c6f93ee6aaecd998fcfb864df3964c6
nhu-pham352 <nhu.pham352@g.austincc.edu>
2021-03-05 23:30:05 -0600

    HW4

--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -1,7 +1,11 @@
 //write includes statements
+#include "loops.h"
+#include<iostream>
+
 
 //write using statements for cin and cout
-
+using std::cout;
+using std::cin;
 
 /*
 Use a do while loop to prompt the user for 
@@ -10,6 +14,27 @@ factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
 {
+	do
+	{
+		int number;
+		cout<<"Enter a number: ";
+		cin>>number;
+
+		while (!(number>=1 && number<=20))
+		{
+			cout<<"Please choose a number from 1 to 20 \n";
+			cout<<"Enter a Number: ";
+			cin>>number;
+		}
+	cout<<"Factorial of "<<number<<" is "<<factorial(number)<<"\n";
+
+	char choice;
+		cout<<"\nEnter Y to continue: ";
+		cin>>choice;
+		if (choice == 'y' || choice == 'Y')
+		cout<<"\n";
+
+	}while(true);
 
 	return 0;
 }
