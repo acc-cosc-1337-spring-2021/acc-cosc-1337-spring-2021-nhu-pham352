d41483374b8e78211c31dc7aa8002043b4a09085
nhu-pham352 <nhu.pham352@g.austincc.edu>
2021-03-26 23:42:01 -0500

    HW 5

--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -1,7 +1,9 @@
 //write include statements
-
+#include"dna.h"
+#include<iostream>
+#include<string>
 //write using statements
-
+using std::string;	using std::cout;	using std::cin;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +14,38 @@ user enters a y or Y.
 */
 int main() 
 {
+	string dna;
+	int option;
+	char choice;
+  do 
+  {
+	cout << "\nEnter 1 to get GC content" << "\n";
+ 	cout << "Enter 2 to get DNA complement" << "\n\n";
+    cout << "Enter your option : ";
+    cin >> option;
+	cout << "Enter a DNA string: ";
+    cin >> dna;
+	
+   	if (option == 1)
+	{
+		double content = get_gc_content(dna);
+		cout<<"GC content: "<<content<<"\n";
+
+	}
+	else if (option == 2)
+	{
+		std::string complement = get_dna_complement(dna);
+		cout<<"DNA Complement: "<<complement<<"\n";
+	}
+	else
+	{
+		cout<<"Invalid option"<<"\n";
+	}
+	
+
+	
+	cout<<"\nEnter Y to countinue: ";
+	cin>>choice;
+	}while (choice == 'Y' ||choice == 'y');
 	return 0;
 }
