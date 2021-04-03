e0d37c09ef2166eee5add4e9736d3933ddf09ad7
nhu-pham352 <nhu.pham352@g.austincc.edu>
2021-04-03 00:03:01 -0500

    HW 6

--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,25 @@
 //h
+#include<string>
+#include <vector>
+
+using std::string;  using std::vector;
+
+//class for tic tac toe game
+class TicTacToe
+{
+    public:
+        bool game_over();
+        void start_game(string first_player);
+        void mark_board(int position);
+        void display_board() const;
+        string get_player() const;
+
+    private:
+        void set_next_player();
+        bool check_board_full();
+        void clear_board();
+
+        string player;
+        vector<string> pegs{9," "};
+
+};
